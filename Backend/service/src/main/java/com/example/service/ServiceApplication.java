d16a2cdb1023fa4fc2118a74ccb22b471c0bd2c0
Gopalan M A <gopalrmd90@gmail.com>
2020-08-20 16:17:05 +0530

    swagger api doc support added

--- Backend/service/src/main/java/com/example/service/ServiceApplication.java
+++ Backend/service/src/main/java/com/example/service/ServiceApplication.java
@@ -1,13 +1,14 @@
 package com.example.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class ServiceApplication {
 
 	public static void main(String[] args) {
